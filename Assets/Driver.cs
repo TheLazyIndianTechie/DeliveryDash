593aab1dbe40484b24d175ff14816cdbca8a3b5f
TheLazyIndianTechie <thelazyindiantechie@gmail.com>
2026-01-13 18:40:01 +0530

    Added rotation and movement with variables

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    float steerSpeed = 0.5f;
+    private float moveSpeed = 0.01f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0,0,steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
