2d49e2a32d8084c95aca340e251056ca0f976632
TheLazyIndianTechie <thelazyindiantechie@gmail.com>
2026-01-13 20:24:22 +0530

    chore: Multiplied movment by Time.deltatime and added two variables local that are moveAmount and steerAmount

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -37,7 +37,10 @@ public class Driver : MonoBehaviour
             steer = -1.0f;
         }
 
-        transform.Translate(0, move * moveSpeed, 0);
-        transform.Rotate(0,0,steer * steerSpeed);
+        float moveAmount = move * moveSpeed * Time.deltaTime;
+        float steerAmount = steer * steerSpeed * Time.deltaTime;
+        
+        transform.Translate(0, moveAmount, 0);
+        transform.Rotate(0,0,steerAmount);
     }
 }
