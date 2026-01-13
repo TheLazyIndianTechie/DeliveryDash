2f87eda61d29540440d9f4200f412d48c08ddfef
TheLazyIndianTechie <thelazyindiantechie@gmail.com>
2026-01-13 19:33:54 +0530

    Added user input and movement in 4 directions

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,18 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 0.5f;
     [SerializeField] private float moveSpeed = 0.01f;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    }
 
-    // Update is called once per frame
+    
     public void Update()
     {
+        if (Keyboard.current.upArrowKey.isPressed|| Keyboard.current.wKey.isPressed)
+        {
+            Debug.Log("We are pushing forward");
+        }
+        
+        else if (Keyboard.current.downArrowKey.isPressed|| Keyboard.current.sKey.isPressed)
+        {
+            Debug.Log("We are pushing backwards");
+        }
+
+        if (Keyboard.current.leftArrowKey.isPressed|| Keyboard.current.aKey.isPressed)
+        {
+            Debug.Log("We are pushing left");
+            
+        }
+
+        else if (Keyboard.current.rightArrowKey.isPressed|| Keyboard.current.dKey.isPressed)
+        {
+            Debug.Log("We are pushing right");
+        }
+        
         transform.Rotate(0,0,steerSpeed);
         transform.Translate(0, moveSpeed, 0);
     }
