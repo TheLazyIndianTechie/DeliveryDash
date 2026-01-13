e68ee68681a5b7eec1edc7bb4438b866438c4080
TheLazyIndianTechie <thelazyindiantechie@gmail.com>
2026-01-13 19:51:06 +0530

    Addeed steering and movement values for controlling the car capsule

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -3,34 +3,41 @@ using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 0.5f;
     [SerializeField] private float moveSpeed = 0.01f;
+    [SerializeField] float steerSpeed = 0.5f;
 
-    
+
     public void Update()
     {
+        float move = 0.0f;
+        float steer = 0.0f;
+
         if (Keyboard.current.upArrowKey.isPressed|| Keyboard.current.wKey.isPressed)
         {
             Debug.Log("We are pushing forward");
+            move = 1.0f;
         }
         
         else if (Keyboard.current.downArrowKey.isPressed|| Keyboard.current.sKey.isPressed)
         {
             Debug.Log("We are pushing backwards");
+            move = -1.0f;
         }
 
         if (Keyboard.current.leftArrowKey.isPressed|| Keyboard.current.aKey.isPressed)
         {
             Debug.Log("We are pushing left");
-            
+            steer = 1.0f;
+
         }
 
         else if (Keyboard.current.rightArrowKey.isPressed|| Keyboard.current.dKey.isPressed)
         {
             Debug.Log("We are pushing right");
+            steer = -1.0f;
         }
-        
-        transform.Rotate(0,0,steerSpeed);
-        transform.Translate(0, moveSpeed, 0);
+
+        transform.Translate(0, move * moveSpeed, 0);
+        transform.Rotate(0,0,steer * steerSpeed);
     }
 }
