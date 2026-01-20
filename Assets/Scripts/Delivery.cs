17851965804acb64318199d3526f01abbfef2023
TheLazyIndianTechie <thelazyindiantechie@gmail.com>
2026-01-20 18:36:49 +0530

    Set condition to avoid pickup up multiple packages until delivered

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -3,15 +3,25 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     private bool _hasPackage;
+    [SerializeField] private float pickupDuration = 0.5f;
+    
     
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Package"))
         {
-            Debug.Log("Package picked up");
-            _hasPackage = true;
+            if (!_hasPackage)
+            {
+                Debug.Log("Package picked up");
+                _hasPackage = true;
+                Destroy(other.gameObject, pickupDuration);
+            }
+            else if (_hasPackage)
+            {
+                Debug.Log("Package already picked up. Deliver this first");
+            }
         }
-
+        
         if (other.CompareTag("Customer") && _hasPackage)
         {
             Debug.Log("Package delivered");
