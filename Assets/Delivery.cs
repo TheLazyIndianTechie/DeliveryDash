8960123d6685047ab0b4e57fb896bd4beb2563bc
TheLazyIndianTechie <thelazyindiantechie@gmail.com>
2026-01-20 17:59:53 +0530

    Added bools for package delivery

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+    private bool _hasPackage;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Package"))
         {
             Debug.Log("Package picked up");
+            _hasPackage = true;
         }
 
-        if (other.CompareTag("Customer"))
+        if (other.CompareTag("Customer") && _hasPackage)
         {
             Debug.Log("Package delivered");
+            _hasPackage = false;
         }
     }
 }
